a3cda49da4e28992d09b5e64e842d7ee7a7c3c90
Jonathan Dai <nightghost5078@gmail.com>
2014-07-28 17:57:28 +0800

    Rename library.

--- src/bin/web.dart
+++ src/bin/web.dart
@@ -3,8 +3,8 @@ library Cart;
 import 'dart:io';
 import 'dart:async';
 import 'dart:convert';
-import 'package:http/http.dart' as libhttp;
-import 'package:start/start.dart' as libstart;
+import 'package:http/http.dart' as LibHttp;
+import 'package:start/start.dart' as LibStart;
 import 'package:google_drive_v2_api/drive_v2_api_console.dart' as GoogleDrive;
 
 import 'package:oauth2/oauth2.dart' as OAuth2;
@@ -20,11 +20,11 @@ main() {
   new File('config/oauth.json').readAsString().then((String contents) {
     oauth = JSON.decode(contents);
 
-    libstart.start(port: 18090).then((libstart.Server app) {
+    LibStart.start(port: 18090).then((LibStart.Server app) {
 
       PinLogger.instance.fine('[EvernoteBlog Web] App server started, listening on port 18090 ...');
 
-      app.get('/error').listen((libstart.Request request) {
+      app.get('/error').listen((LibStart.Request request) {
         var err = new ArgumentError('custom error in handler');
 
         try {
@@ -36,17 +36,17 @@ main() {
         }
       });
 
-      app.get('/counter').listen((libstart.Request request) {
+      app.get('/counter').listen((LibStart.Request request) {
         request.response.header('Content-Type', 'application/json; charset=UTF-8').json(
             {'counter': counter++}
         );
       });
 
-      app.get('/blog').listen((libstart.Request request) {
+      app.get('/blog').listen((LibStart.Request request) {
         request.response.json(Uri.splitQueryString(request.uri.query));
       });
 
-      app.get('/oauth2').listen((libstart.Request request) {
+      app.get('/oauth2').listen((LibStart.Request request) {
         var grant = new OAuth2.AuthorizationCodeGrant(
             oauth['web']['client_id'],
             oauth['web']['client_secret'],
@@ -60,13 +60,13 @@ main() {
         request.response.redirect(authUrl.toString());
       });
 
-      app.get('/oauth2next').listen((libstart.Request request) {
+      app.get('/oauth2next').listen((LibStart.Request request) {
         // error: https://oauth2-login-demo.appspot.com/code?error=access_denied
         // succeed: https://oauth2-login-demo.appspot.com/code?code=4/P7q7W91a-oMsCeLvIaQm6bTrgtp7
         Map queries = Uri.splitQueryString(request.uri.query);
         if (queries.containsKey('code')) {
           // oauth code got, process next step
-          libhttp.post(
+          LibHttp.post(
               oauth['google_oauth_token_url'],
               body: {
                 'code': queries['code'],
@@ -75,7 +75,7 @@ main() {
                 'redirect_uri': oauth['web']['redirect_url'],
                 'grant_type': 'authorization_code'
               }
-          ).then((libhttp.Response response) {
+          ).then((LibHttp.Response response) {
             // token got
 //            {
 //              "access_token" : "ya29.UACD48pvUVNbBxwAAAB-QhCLdx1oGiKa2TLray9FYM6QdFcyeCSnLAMAw6uqXA",
