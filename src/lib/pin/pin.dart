1ca66b87205f75f2ea33ebeab45a0de36701e9c1
Jonathan Dai <nightghost5078@gmail.com>
2014-07-30 12:52:32 +0800

    Fix third party lib name.

--- src/lib/pin/pin.dart
+++ src/lib/pin/pin.dart
@@ -1,8 +1,13 @@
 library pin;
 
 import 'dart:io';
-import 'package:path/path.dart' as libpath;
+
+import 'package:path/path.dart' as LibPath;
 import 'package:logging/logging.dart';
+import 'package:google_oauth2_client/google_oauth2_console.dart' as GoogleOAuth;
 
 part 'utility/logger.dart';
 part 'utility/utility.dart';
+
+part 'google/oauth.dart';
+part 'google/drive.dart';
--- src/lib/pin/utility/utility.dart
+++ src/lib/pin/utility/utility.dart
@@ -3,7 +3,7 @@ part of pin;
 class PinUtility {
 
   static void setCwdToRoot([String relative = '']) {
-    String root = libpath.join(libpath.dirname(Platform.script.toFilePath()), relative);
+    String root = LibPath.join(LibPath.dirname(Platform.script.toFilePath()), relative);
 
     PinLogger.instance.fine('[PinUtility] Set Directory.current to: $root');
 
