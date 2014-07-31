bb18a8e51fc46f9f8abd2a89803702d1fef43650
Jonathan Dai <nightghost5078@gmail.com>
2014-07-31 15:11:03 +0800

    Add client app.js

---
+++ web/src/scripts/app.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var $ = require('jquery');
+var moment = require('moment');
+require('angular');
+require('angular-route');
+require('angular-animate');
+
+var app = angular.module('Cart', [
+    'ngAnimate', 'ngRoute',
+    'Cart.Controllers', 'Cart.Services'
+]);
+
+var controllers = angular.module('Cart.Controllers', []);
+
+controllers.controller('CartIndexCtrl', ['$scope', function ($scope) {
+}]);
+
+var services = angular.module('Cart.Services', []);
+
+services.factory('CartIndexService', ['$http', '$q', function($http, $q) {
+    return {
+    };
+}]);
+
+var $html = angular.element(document.getElementsByTagName('html')[0]);
+angular.element().ready(function() {
+    angular.bootstrap($html, ['Cart']);
+});
