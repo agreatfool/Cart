1fff007a48ecf2ae41876e12870c8e59119a3dca
Jonathan Dai <nightghost5078@gmail.com>
2014-08-04 13:14:00 +0800

    Remove browserify bash script & add gulp startup script & add jshint config file.

--- bin/browserfiy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-browserify -t debowerify -t deamdify ${SCRIPT_DIR}/../web/src/scripts/app.js -o ${SCRIPT_DIR}/../web/public/js/bundle.js
---
+++ gulp.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd ${SCRIPT_DIR}
+export NODE_ENV=development
+gulp
