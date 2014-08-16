1f6dfcaadd090d7b57bd622f67c65e96a3d6ebe0
Jonathan Dai <nightghost5078@gmail.com>
2014-08-16 23:05:37 +0800

    Calendar logic done.

--- web/src/scripts/controller/CartSearchCtrl.js
+++ web/src/scripts/controller/CartSearchCtrl.js
@@ -4,38 +4,33 @@
 module.exports = function ($scope, $compile) {
     console.log('CartSearchCtrl');
 
-    // calendar
+    // CALENDAR
     var date = new Date();
     var d = date.getDate();
     var m = date.getMonth();
     var y = date.getFullYear();
 
-    /* event source that contains custom events on the scope */
-    $scope.events = [
-        {title: 'All Day Event',start: new Date(y, m, 1)},
-        {title: 'Long Event',start: new Date(y, m, d - 5),end: new Date(y, m, d - 2)},
-        {id: 999,title: 'Repeating Event',start: new Date(y, m, d - 3, 16, 0),allDay: false},
-        {id: 999,title: 'Repeating Event',start: new Date(y, m, d + 4, 16, 0),allDay: false},
-        {title: 'Birthday Party',start: new Date(y, m, d + 1, 19, 0),end: new Date(y, m, d + 1, 22, 30),allDay: false},
-        {title: 'Click for Google',start: new Date(y, m, 28),end: new Date(y, m, 29),url: 'http://google.com/'}
-    ];
-    /* event source that calls a function on every view switch */
-    $scope.eventsF = function (start, end, callback) {
-        console.log('eventsF');
-        var s = new Date(start).getTime() / 1000;
-        var e = new Date(end).getTime() / 1000;
-        var m = new Date(start).getMonth();
-        var events = [{title: 'Feed Me ' + m,start: s + (50000),end: s + (100000),allDay: false, className: ['customFeed']}];
-        callback(events);
-    };
+    $scope.events = [{
+        title: 'Blog Post 1',
+        start: new Date(y, m, d, 9, 56, 43),
+        allDay: false,
+        url: "http://www.google.com",
+        className: ["calEvent"]
+    }, {
+        title: 'This is a very very long long long Blog Post title',
+        start: new Date(y, m, d - 1, 18, 56, 43),
+        allDay: false,
+        url: "http://www.google.com",
+        className: ["calEvent"]
+    }];
 
-    /* add custom event*/
-    $scope.addEvent = function() {
+    $scope.addEvent = function(title, date, url) {
         $scope.events.push({
-            title: 'Open Sesame',
-            start: new Date(y, m, 28),
-            end: new Date(y, m, 29),
-            className: ['openSesame']
+            title: title,
+            start: date,
+            allDay: false,
+            url: url,
+            className: ["calEvent"]
         });
     };
 
