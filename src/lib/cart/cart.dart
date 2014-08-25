626ddcba5f9615a809267eb60b4f780e90f897c9
Jonathan Dai <nightghost5078@gmail.com>
2014-08-25 17:46:48 +0800

    Show dependencies in cart.dart.

--- src/lib/cart/cart.dart
+++ src/lib/cart/cart.dart
@@ -4,7 +4,10 @@ import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:markdown/markdown.dart';
+
 import '../pin/pin.dart';
 
 part 'const/const.dart';
 part 'model/model.dart';
+part 'system.dart';
