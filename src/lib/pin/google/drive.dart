ca79d9054c6d0fdf6ee187b0705538f58a55abbe
Jonathan Dai <nightghost5078@gmail.com>
2014-08-27 17:23:19 +0800

    Add list all files high level drive api.

--- src/lib/pin/google/drive.dart
+++ src/lib/pin/google/drive.dart
@@ -2,6 +2,8 @@ part of pin;
 
 class PinGoogleDrive {
 
+  static const String qRootFiles = "'root' in parents";
+
   GoogleDrive.Drive _drive;
   PinGoogleOAuth _oauth;
 
@@ -10,7 +12,43 @@ class PinGoogleDrive {
     _drive.makeAuthRequests = true;
   }
 
-  Future<HashMap> listFiles({HashMap queryParams: null}) {
+  //-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+  //-* HIGH LEVEL APIs
+  //-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+  Future<List> listAll({HashMap queryParams: null}) {
+    var files = new List();
+    final completer = new Completer();
+
+    Future<HashMap> listPageOfFiles(HashMap response) {
+      // handle response files data
+      List items = response['items'];
+      if (items.length > 0) {
+        files.addAll(items);
+      }
+      if (!response.containsKey('nextPageToken')) {
+        // this is the final page, finish the future
+        completer.complete(files);
+      } else {
+        // still has next page, retrieve it
+        HashMap params = {
+          'pageToken': response['nextPageToken']
+        };
+        params.addAll(queryParams);
+        drive_list(queryParams: params)
+          .then((nextResponse) => listPageOfFiles(nextResponse));
+      }
+    }
+
+    drive_list(queryParams: queryParams)
+      .then((response) => listPageOfFiles(response));
+
+    return completer.future;
+  }
+
+  //-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+  //-* DRIVE ORIGINAL APIs
+  //-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+  Future<HashMap> drive_list({HashMap queryParams: null}) {
     return _drive.request('files', 'GET', queryParams: queryParams);
   }
 
