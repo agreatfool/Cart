b0716a883ed951d8fa69b90a27462d1e6decf18f
Jonathan Dai <nightghost5078@gmail.com>
2014-09-03 09:22:32 +0800

    Adjust lib order.

--- src/lib/cart/cart.dart
+++ src/lib/cart/cart.dart
@@ -5,8 +5,9 @@ import 'dart:collection';
 import 'dart:convert';
 import 'dart:io';
 
-import 'package:markdown/markdown.dart';
+import 'package:google_drive_v2_api/drive_v2_api_client.dart' as GoogleDriveClient show File, FileList;
 import 'package:http/http.dart' as LibHttp;
+import 'package:markdown/markdown.dart';
 import 'package:path/path.dart' as LibPath;
 
 import '../pin/pin.dart';
