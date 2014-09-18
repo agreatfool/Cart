84a61395e46528504f1bb8d89ae49f3157525575
Jonathan Dai <nightghost5078@gmail.com>
2014-09-18 13:42:11 +0800

    Add cart app utility js code file.

---
+++ web/src/scripts/utility.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/* global $ */
+//var
+
+//-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+//-* UTILITY
+//-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+var CartUtility = function() {};
+
+CartUtility.prototype.spinShow = function() {
+
+};
+
+CartUtility.prototype.spinHide = function() {
+
+};
+
+module.exports = new CartUtility();
