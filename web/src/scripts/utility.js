056ae23316876d6a3b75025d1431689e99058b7c
Jonathan Dai <nightghost5078@gmail.com>
2014-09-19 11:05:46 +0800

    Add spin related logics & add pnotify function.

--- web/src/scripts/utility.js
+++ web/src/scripts/utility.js
@@ -1,19 +1,56 @@
 'use strict';
 
-/* global $ */
-//var
+/* global _, $, PNotify */
+var CartUtility = function() {
+    this.spinTasksCount = 0;
+    this.pnotifyStack = {"dir1": "up", "dir2": "right", "push": "bottom", "spacing1": 0, "spacing2": 0};
+};
 
-//-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
-//-* UTILITY
-//-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
-var CartUtility = function() {};
-
-CartUtility.prototype.spinShow = function() {
+CartUtility.prototype.spinShow = function(promise) {
+    var self = this;
+    self.spinTasksCount++;
     $('.spinner-mask').show();
+    if (typeof promise === 'object' && promise.hasOwnProperty('finally')) {
+        // this is a promise
+        promise['finally'](function() {
+            self.spinHide();
+        });
+    } else {
+        setTimeout(function() {
+            self.spinHide();
+        }, 300);
+    }
 };
 
 CartUtility.prototype.spinHide = function() {
+    this.spinTasksCount--;
+    if (this.spinTasksCount > 0) {
+        return; // still spin tasks left
+    }
     $('.spinner-mask').hide();
 };
 
+CartUtility.prototype.notify = function(title, text, type, duration) {
+    if (typeof type === 'undefined' || type === '' || type === null) {
+        type = 'info';
+    }
+    if (typeof duration === 'undefined' || duration === '' || duration === null) {
+        duration = 5000; // 5s
+    }
+
+    var options = {
+        title: title,
+        text: text,
+        type: type,
+        delay: duration,
+        addclass: 'stack-bar-bottom',
+        width: "70%",
+        stack: this.pnotifyStack
+    };
+    if (type === 'error') {
+        options['hide'] = false;
+    }
+
+    new PNotify(options);
+};
 module.exports = new CartUtility();
