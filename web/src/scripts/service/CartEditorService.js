1df411d5aac237fa2939192db9d35f6c9df353ca
Jonathan Dai <nightghost5078@gmail.com>
2014-09-28 18:07:33 +0800

    Save editor work.

--- web/src/scripts/service/CartEditorService.js
+++ web/src/scripts/service/CartEditorService.js
@@ -1,9 +1,34 @@
 'use strict';
 
-/* global $, ace, marked */
+/* global _, $, ace, marked, hljs */
 module.exports = function() {
 
+    // markd
+    var renderer = new marked.Renderer();
+    renderer.heading = function(text, level) { // add anchor link
+        return '' +
+            '<h' + level + '>' +
+                '<a name="' + encodeURIComponent(text) + '"></a>' +
+                text +
+            '</h' + level + '>';
+    };
+    marked.setOptions({
+        renderer: renderer,
+        gfm: true,
+        tables: true,
+        breaks: true,
+        pedantic: false,
+        sanitize: true,
+        smartLists: true,
+        smartypants: false,
+        highlight: function (code, lang) {
+            // since highlight lib added class name not work, have to wrap code tag by self
+            return '<code class="hljs ' + lang + '">' + hljs.highlightAuto(code, [lang]).value + '</code>';
+        }
+    });
+
     var createEditor = function(editElementId, previewElementId) {
+        // ACE
         if (typeof editElementId === 'undefined' || editElementId === null || editElementId === '') {
             editElementId = 'markdown-edit';
         }
@@ -29,71 +54,62 @@ module.exports = function() {
         aceEditor.getSession().setUseWrapMode(true);
         aceEditor.session.setScrollTop(false);
         aceEditor.resize(true);
-        //aceEditor.setFontSize("13px");
         aceEditor.getSession().on('change', function() {
-            var editElement = $('#' + editElementId);
-            var previewElement = $('#' + previewElementId);
-            previewElement.html(marked(aceEditor.getSession().getValue()));
-            editElement.css('height', previewElement.css('height'));
+            previewMd(aceEditor);
         });
 
-        marked.setOptions({
-            highlight: function (code, lang) {
-                console.log(lang);
-                console.log(code);
-                //return require('highlight.js').highlightAuto(code).value;
-            }
-        });
-
-        //editor = ace.edit("editor"), window._editor = new Markdown.Editor({}), _editor.run(editor), core = {}, _editor.hooks.set("insertLinkDialog", function(a) {
-        //    return core.insertLinkCallback = a, utils.resetModalInputs(), $(".modal-insert-link").modal(), !0
-        //}), _editor.hooks.set("insertImageDialog", function(a) {
-        //    return core.insertLinkCallback = a, core.catchModal ? !0 : (utils.resetModalInputs(), $(".modal-insert-image").modal(), !0)
-        //}), _editor.hooks.set("onPreviewRefresh", function() {
-        //    return previewMd(), !0
-        //}), $(".action-insert-link").click(function(a) {
-        //    var b = utils.getInputTextValue($("#input-insert-link"), a);
-        //    void 0 !== b && (core.insertLinkCallback(b), core.insertLinkCallback = void 0)
-        //}), $(".action-insert-image").click(function(a) {
-        //    var b = utils.getInputTextValue($("#input-insert-image"), a);
-        //    b && Vine.trigger("ACTION_INSERT_IMAGE", b.trim())
-        //}), $(".modal-insert-link, .modal-insert-image").on("hidden.bs.modal", function() {
-        //    void 0 !== core.insertLinkCallback && (core.insertLinkCallback(null), core.insertLinkCallback = void 0)
-        //}), editor.on("focus", function() {
-        //    Session.resume()
-        //}), editor.on("blur", function() {
-        //    Session.pause()
-        //}), $(document).ready(function() {
-        //    !function(a) {function b(b) {
-        //        var c = a.lines[b];
-        //        0 !== c.length && /heading.*multi/.test(c[0].type) && b && a.lines[b - 1].map(function(a) {
-        //            a.type = c[0].type + ".prev"
-        //        })
-        //    }function c() {
-        //        if (a.running) {
-        //            for (var d = new Date, e = a.currentLine || 0, f = -1, g = a.doc; a.lines[e];) e++;
-        //            var h = e,
-        //                i = g.getLength(),
-        //                j = 0;
-        //            for (a.running = !1; i > e;) {
-        //                a.$tokenizeRow(e), f = e;
-        //                do b(e), e++;
-        //                while (a.lines[e]);
-        //                if (j++, j % 5 === 0 && new Date - d > 20) return a.running = setTimeout(c, 20), void(a.currentLine = e)
-        //            }
-        //            a.currentLine = e, f >= h && a.fireUpdateEvent(h, f)
-        //        }
-        //    }
-        //        a.$worker = function() {
-        //            a.lines.splice(0, a.lines.length), a.states.splice(0, a.states.length), a.currentLine = 0, c()
-        //        }
-        //    }(editor.session.bgTokenizer)
-        //})
+        aceEditor.editElement = $('#' + editElementId);
+        aceEditor.previewElement = $('#' + previewElementId);
 
         return aceEditor;
     };
 
+    var previewMd = function(editor) {
+        var content = editor.getSession().getValue();
+        var converted = marked(content);
+
+        editor.previewElement.html(converted);
+
+        var editHeight = editor.editElement.css('height');
+        var previewHeight = editor.previewElement.css('height');
+        if (editHeight > previewHeight) {
+            editor.previewElement.css('height', editHeight);
+        } else if (editHeight < previewHeight) {
+            editor.editElement.css('height', previewHeight);
+        }
+
+        buildMdToc(converted);
+    };
+
+    var buildMdToc = function(html) { // TODO
+        var tocArray = {1: [], 2: [], 3: [], 4: [], 5: [], 6: []};
+
+        var prevLevel = null;
+        var appendHeader = function(headerLevel, headerText, anchorName) {
+            if (prevLevel == null) {
+                tocArray.push([headerText, anchorName]);
+            } else {
+
+            }
+        };
+
+        if (typeof html !== 'undefined' && html !== null && html !== '') {
+            var headers = $(html).filter(':header');
+            _.forEach(headers, function(header) {
+                var hElement = $(header);
+                var headerLevel = parseInt(hElement.prop("tagName").substring(1));
+                var headerText = hElement.text();
+                var anchorName = hElement.find('a').attr('name');
+            });
+            $('<ol></ol>');
+        }
+
+        return toc;
+    };
+
     return {
-        'createEditor': createEditor
+        'createEditor': createEditor,
+        'previewMd': previewMd,
+        'buildMdToc': buildMdToc
     };
 };
