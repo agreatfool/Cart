65fd1d9e631ad20f2ca95b109ff0ce807933c0a7
Jonathan Dai <nightghost5078@gmail.com>
2014-10-10 17:52:40 +0800

    Post save api done.

--- web/src/scripts/service/CartDataService.js
+++ web/src/scripts/service/CartDataService.js
@@ -1,11 +1,19 @@
 'use strict';
 
-/* global CartUtility */
+/* global PouchDB, CartUtility */
 module.exports = function($http, $q) {
+    var db = new PouchDB('Cart');
+
+    //-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+    //-* MODELS
+    //-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
     var posts = {};
     var categories = {};
     var tags = {};
 
+    //-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+    //-* DATA RELATED
+    //-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
     var getInitData = function() {
         var deferred = $q.defer();
         $http({
@@ -25,7 +33,65 @@ module.exports = function($http, $q) {
         return CartUtility.spinShow(deferred.promise);
     };
 
+    //-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+    //-* DB RELATED
+    //-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+    var dbSave = function(uuid, markdown) {
+        markdown = {
+            "md": markdown
+        };
+        var deferred = $q.defer();
+        db.get(uuid)
+        .then(function(doc) {
+            return db.put(markdown, uuid, doc._rev); // old doc found, update it
+        }, function(err) {
+            if (typeof err === 'object' && err.status === 404) { // old doc not found, just create new
+                return db.put(markdown, uuid);
+            } else if (typeof err === 'object') {
+                CartUtility.notify('Error', err.toString(), 'error');
+                var sub = $q.defer();
+                sub.resolve(false); // error encountered, resolve with false
+                return sub.promise;
+            }
+        })
+        .then(function(response) {
+            if (response !== false) {
+                deferred.resolve(true); // previous done with no error, resolve with true
+            } else {
+                deferred.resolve(false); // previous done with error, resolve with false
+            }
+        }, function(err) {
+            if (typeof err === 'object') {
+                CartUtility.notify('Error', err.toString(), 'error');
+                deferred.resolve(false); // error, popup notification, resolve with false
+            } else {
+                deferred.resolve(true); // no error, resolve with true
+            }
+        });
+        return deferred.promise;
+    };
+
+    //-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+    //-* POST RELATED
+    //-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+    var postSaveTmp = function(uuid, markdown) {
+        dbSave(uuid, markdown).then(function(result) {
+            if (false === result) {
+                CartUtility.notify('Save', 'Failed in saving!', 'error');
+            } else {
+                CartUtility.notify('Save', 'Post saved!');
+            }
+        });
+    };
+
+    // FIXME remove later
+    global.db = db;
+    global.dbSave = dbSave;
+    global.postSaveTmp = postSaveTmp;
+
     return {
-        'getInitData': getInitData
+        'getInitData': getInitData,
+        // post APIs
+        'postSaveTmp': postSaveTmp
     };
 };
