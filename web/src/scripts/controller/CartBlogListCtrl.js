1c8b1c85b7656086c76247658570baac5e31f9a8
Jonathan Dai <nightghost5078@gmail.com>
2014-10-30 10:36:42 +0800

    Add list page functionality button privilege control.

--- web/src/scripts/controller/CartBlogListCtrl.js
+++ web/src/scripts/controller/CartBlogListCtrl.js
@@ -4,7 +4,12 @@
 module.exports = function ($scope, $location, $window) {
     CartUtility.log('CartBlogListCtrl');
 
+    $scope.isMaster = $accessService.isMaster();
+
     $scope.createNew = function() {
+        if (!$scope.isMaster) {
+            return;
+        }
         // redirect to create post page
         $window.location.href = CartUtility.getPureRootUrlFromLocation($location) + 'new/' + uuid.v4();
     };
