4061c0abc8c460c1b1d7bbe73d9523169471d6c2
Jonathan Dai <nightghost5078@gmail.com>
2014-11-18 17:11:01 +0800

    Save post list page update.

--- web/src/scripts/controller/CartBlogListCtrl.js
+++ web/src/scripts/controller/CartBlogListCtrl.js
@@ -1,8 +1,10 @@
 'use strict';
 
-/* global uuid, CartUtility */
+/* global _, uuid, CartUtility */
 module.exports = function ($scope, $location, $window, $routeParams, $modal, $dataService, $accessService) {
     CartUtility.log('CartBlogListCtrl');
+
+    $scope.utility = CartUtility;
 
     $scope.isMaster = $accessService.isMaster();
 
@@ -25,7 +27,52 @@ module.exports = function ($scope, $location, $window, $routeParams, $modal, $da
         });
     };
 
-    $scope.postsOnPage = [];
+    $scope.fetchCategoryDisplayName = function(categoryUuid) {
+        var name = '';
+        var category = $dataService.categorySearchById(categoryUuid);
+        if (!_.isNull(category)) {
+            name = category.title;
+        }
+        return name;
+    };
+
+    $scope.fetchTagDisplayName = function(tagUuid) {
+        var name = '';
+        var tag = $dataService.tagSearchById(tagUuid);
+        if (!_.isNull(tag)) {
+            name = tag.title;
+        }
+        return name;
+    };
+
+    $scope.deletePost = function(uuid) {
+        if (!window.confirm('Do you confirm the delete operation of the post: ' + uuid + '?\nThis action cannot be undo!')) {
+            return;
+        }
+        var post = $dataService.postSearchById(uuid);
+        if (_.isNull(post)) {
+            CartUtility.notify('Error!', 'Target post to be deleted not found, uuid: ' + uuid, 'error');
+            return;
+        }
+        $dataService.postRemove(uuid).then(function() {
+            CartUtility.notify('Done!', 'Post ' + uuid + ' deleted!');
+        });
+    };
+
+    var postRows = [];
+
+    // pagination
+    $scope.postOnPage = []; // data used to be displayed on page
+    $scope.paginationTotalItems = 0;
+    $scope.paginationCurrentPage = 1;
+    $scope.itemsPerPage = 10;
+    $scope.paginationMaxSize = 5;
+
+    $scope.loadPageData = function() {
+        var startPos = ($scope.paginationCurrentPage - 1) * $scope.itemsPerPage;
+        var endPos = startPos + $scope.itemsPerPage;
+        $scope.postOnPage = postRows.slice(startPos, endPos);
+    };
 
     var displayPageItems = function() {
         var options = {};
@@ -44,7 +91,11 @@ module.exports = function ($scope, $location, $window, $routeParams, $modal, $da
         if ($location.url().indexOf('day') !== -1) {
             options.day = $routeParams.datetime;
         }
-        $scope.postsOnPage = $dataService.postSearch(options);
+        postRows = $dataService.postSearch(options);
+        postRows = _.sortBy(postRows, function(post) { return post.created; }).reverse(); // orderBy "created" DESC
+        $scope.paginationTotalItems = postRows.length;
+        $scope.paginationCurrentPage = 1;
+        $scope.loadPageData();
     };
     displayPageItems();
 };
