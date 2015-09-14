e8306057a65889f384caa24933c71f8c8ec71854
Jonathan Dai <nightghost5078@gmail.com>
2015-09-14 16:06:23 +0800

    Change default action of path '/', send index.html file.

--- src/server/boot/root.js
+++ src/server/boot/root.js
@@ -1,6 +1,9 @@
 module.exports = function(server) {
   // Install a `/` route that returns server status
   var router = server.loopback.Router();
-  router.get('/', server.loopback.status());
+  //router.get('/', server.loopback.status()); // loopback default router, change it to do production work
+  router.get('/', function(req, res) {
+    res.sendfile('client/public/index.html');
+  });
   server.use(router);
 };
