d36a14afb76f399be21a0fc2dcafdb9f79b09cde
Jonathan Dai <nightghost5078@gmail.com>
2015-09-14 16:32:08 +0800

    Test routers.

--- src/client/es6/routers.js
+++ src/client/es6/routers.js
@@ -1,4 +1,41 @@
 function CartRouters($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/list');
+
+  $stateProvider
+    .state('list', {
+      url: '/list',
+      templateUrl: 'views/list.html',
+      controller: 'CartBlogList'
+    })
+    .state('config', {
+      url: '/config',
+      templateUrl: 'views/config.html'
+    });
+
+  //$stateProvider
+  //  .state('init', {
+  //    url: '/init',
+  //    templateUrl: "route1.html"
+  //  })
+  //  .state('route1.list', {
+  //    url: "/list",
+  //    templateUrl: "route1.list.html",
+  //    controller: function($scope){
+  //      $scope.items = ["A", "List", "Of", "Items"];
+  //    }
+  //  })
+  //
+  //  .state('route2', {
+  //    url: "/route2",
+  //    templateUrl: "route2.html"
+  //  })
+  //  .state('route2.list', {
+  //    url: "/list",
+  //    templateUrl: "route2.list.html",
+  //    controller: function($scope){
+  //      $scope.things = ["A", "Set", "Of", "Things"];
+  //    }
+  //  })
 }
 
 CartRouters.$inject = ['$stateProvider', '$urlRouterProvider'];
