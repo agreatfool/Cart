8789732b4ceb906ecc37aa56ba185b52b7c94128
Jonathan Dai <nightghost5078@gmail.com>
2015-09-17 21:27:20 +0800

    Add more filter rules to meet requirement.

--- src/server/boot/angular-html5.js
+++ src/server/boot/angular-html5.js
@@ -12,6 +12,18 @@ module.exports = function supportAngularHtml5(server) {
     } else if (reqUrl.match(/^\/views\/.*/)) {
       // static html view resources, skip it
       next();
+    } else if (reqUrl.match(/^\/img\/.*/)) {
+      // static image resources, skip it
+      next();
+    } else if (reqUrl.match(/^\/page\/.*/)) {
+      // static html resources, skip it
+      next();
+    } else if (reqUrl.match(/^\/api.*/)) {
+      // loopback api root, skip it
+      next();
+    } else if (reqUrl.match(/^\/explorer.*/)) {
+      // loopback explorer root, skip it
+      next();
     } else {
       // otherwise just gave the index.html for angular HTML5 mode support
       res.sendFile('client/public/index.html', { root: libPath.join(__dirname, '..', '..') });
